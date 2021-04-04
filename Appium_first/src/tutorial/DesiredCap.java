9d3adf4baf12cd8d477691228379434a9fca53a4
Anju <anjukumaris@gmail.com>
2021-04-04 01:36:59 -0400

    Create Object- DesiredCapability Connection- Invoking Android Server

--- Appium_first/src/tutorial/DesiredCap.java
+++ Appium_first/src/tutorial/DesiredCap.java
@@ -1,9 +1,13 @@
 package tutorial;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class DesiredCap {
@@ -11,10 +15,18 @@ public class DesiredCap {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		File f=new File("src");
-		File file=new File(f,"")
+		File file=new File(f,"ApiDemos-debug.apk");
 		
 		DesiredCapabilities cap=new DesiredCapabilities();
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME,"emulator1");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME,file.getAbsolutePath());
+		//AndroidDriver<AndroidElement> driver=new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+		try {
+			AndroidDriver driver=new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 
 	}
