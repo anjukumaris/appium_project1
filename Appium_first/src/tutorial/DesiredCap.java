d390c4dc2f04e95c08a515181792b37927c32e7f
Anju <anjukumaris@gmail.com>
2021-04-04 01:34:35 -0400

    Desired Capabilities Connections- Invoking Android Server

--- Appium_first/src/tutorial/DesiredCap.java
+++ Appium_first/src/tutorial/DesiredCap.java
@@ -1,12 +1,20 @@
 package tutorial;
 
+import java.io.File;
+
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.remote.MobileCapabilityType;
 
 public class DesiredCap {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		File f=new File("src");
+		File file=new File(f,"")
+		
 		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME,"emulator1");
 		
 
 	}
