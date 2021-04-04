13f3d720de8215829eba13c88c769e635ac77541
Anju <anjukumaris@gmail.com>
2021-04-04 00:18:34 -0400

    First Commit

---
+++ Appium_first/src/tutorial/DesiredCap.java
@@ -0,0 +1,14 @@
+package tutorial;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class DesiredCap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DesiredCapabilities cap=new DesiredCapabilities();
+		
+
+	}
+
+}
